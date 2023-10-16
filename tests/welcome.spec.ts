4c7d9765f43836180481c78f1f92d7e00890c2b1
Tomasz Borzecki <tomaszbor4@gmail.com>
2023-10-16 16:33:56 +0200

    Welcome screen tests started

--- tests/welcome.spec.ts
+++ tests/welcome.spec.ts
@@ -1,28 +1,75 @@
 import { test, expect } from "@playwright/test";
 
-test.only("Successful related cards routing", async ({ page }) => {
+test("Successful related cards routing", async ({ page }) => {
   //arrange
   const baseURL = "https://angular-qa-recruitment-app.netlify.app/";
 
   //act
   await page.goto(baseURL);
 
-  const page1Promise = page.waitForEvent("popup");
-
+  const learnPagePromise = page.waitForEvent("popup");
   await page.getByRole("link", { name: "Learn Angular" }).click();
-  const page1 = await page1Promise;
-  const page2Promise = page.waitForEvent("popup");
+  const learnPage = await learnPagePromise;
+  const CLIPagePromise = page.waitForEvent("popup");
   await page.getByRole("link", { name: "CLI Documentation" }).click();
-  const page2 = await page2Promise;
-  const page3Promise = page.waitForEvent("popup");
+  const CLIPage = await CLIPagePromise;
+  const blogPagePromise = page.waitForEvent("popup");
   await page.getByRole("link", { name: "Angular Blog" }).click();
-  const page3 = await page3Promise;
-  const page4Promise = page.waitForEvent("popup");
+  const blogPage = await blogPagePromise;
+  const devToolsPagePromise = page.waitForEvent("popup");
   await page.getByRole("link", { name: "Angular DevTools" }).click();
-  const page4 = await page4Promise;
+  const devToolsPage = await devToolsPagePromise;
+  await page.goto(baseURL);
 
   //assert
-  await expect(page.locator("span _ngcontent-rcf-c24")).toHaveText(
-    "Recruitment app is running!"
-  );
+  await expect(learnPage).toHaveURL("https://angular.io/tutorial");
+  await expect(CLIPage).toHaveURL("https://angular.io/cli");
+  await expect(blogPage).toHaveURL("https://blog.angular.io/");
+  await expect(devToolsPage).toHaveURL("https://angular.io/guide/devtools");
+});
+
+test.describe.only("Successful dynamic terminal prompts test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://angular-qa-recruitment-app.netlify.app/");
+  });
+
+  test("New component test", async ({ page }) => {
+    //arrange
+    //act
+    await page.getByRole("button", { name: "New Component" }).click();
+
+    //assert
+    await expect(page.locator(".terminal")).toHaveText(
+      "ng generate component xyz"
+    );
+  });
+
+  test("Angular Material test", async ({ page }) => {
+    //arrange
+    //act
+    await page.getByRole("button", { name: "Angular Material" }).click();
+
+    //assert
+    await expect(page.locator(".terminal")).toHaveText(
+      "ng add @angular/material"
+    );
+  });
+
+  test("Add PWA Support test", async ({ page }) => {
+    //arrange
+    //act
+    await page.getByRole("button", { name: "Add PWA Support" }).click();
+
+    //assert
+    await expect(page.locator(".terminal")).toHaveText("ng add @angular/pwa");
+  });
+
+  test("Add Dependency test", async ({ page }) => {
+    //arrange
+    //act
+    await page.getByRole("button", { name: "Add Dependency" }).click();
+
+    //assert
+    await expect(page.locator(".terminal")).toHaveText("ng add _____");
+  });
 });
