ccc05e5b1a848f3b6311592e7c925f1ae02870b6
Tomasz Borzecki <tomaszbor4@gmail.com>
2023-10-16 20:01:13 +0200

    Form fields validation continuation

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -4,22 +4,29 @@ test.describe("Form fields validation", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://angular-qa-recruitment-app.netlify.app/form");
   });
-  test.only("Empty name field validation", async ({ page }) => {
-
+  test("Empty name field validation", async ({ page }) => {
     //arrange
 
     //act
     await page.goto("https://angular-qa-recruitment-app.netlify.app/");
     await page.getByRole("link", { name: "Form" }).click();
     await page.getByLabel("Name").fill("");
-    await page.getByText("Name is required").click();
 
     //assert
-    await expect(page.getByText("Name is required")).toHaveText(
-      "Name is required"
-    );
+    await expect(page.getByText("Name is required")).toBeVisible();
   });
 
+  test.only("Special chars in name field validation", async ({ page }) => {
+    //arrange
 
-  
+    //act
+    await page.goto("https://angular-qa-recruitment-app.netlify.app/");
+    await page.getByRole("link", { name: "Form" }).click();
+    await page.getByLabel("Name").fill("123./!");
+
+    //assert
+    await expect(
+      page.getByText("Special characters not allowed")
+    ).toBeVisible();
+  });
 });
